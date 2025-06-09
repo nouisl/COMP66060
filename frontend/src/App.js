b0f597804e19d2b40b020177d2ac566bf5007695
nouisl <78652710+nouisl@users.noreply.github.com>
2025-06-09 23:56:30 +0100

    updated App.js to have routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// import styles and components
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Web3ProviderWrapper } from './context/Web3Context';
 import Home from './components/Home';
@@ -7,27 +8,33 @@ import DocumentUpload from './components/DocumentUpload';
 import DocumentList from './components/DocumentList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { NotificationProvider } from '@web3uikit/core';
 
-
+// App component
 function App() {
   return (
-    <Router>
-      <Web3ProviderWrapper>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/upload" element={<DocumentUpload />} />
-              <Route path="/documents" element={<DocumentList />} />
-              <Route path="/register" element={<UserRegistration />} />
-              <Route path="/profile" element={<UserProfile />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Web3ProviderWrapper>
-    </Router>
+    <div className="App">
+      <NotificationProvider>
+        <Web3ProviderWrapper>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-grow container mx-auto px-4 py-8">
+              <Router>
+                {/* Defining routes */}
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/upload" element={<DocumentUpload />} />
+                  <Route path="/documents" element={<DocumentList />} />
+                  <Route path="/register" element={<UserRegistration />} />
+                  <Route path="/profile" element={<UserProfile />} />
+                </Routes>
+              </Router>
+            </main>
+            <Footer />
+          </div>
+        </Web3ProviderWrapper>
+      </NotificationProvider>
+    </div>
   );
 }
 
