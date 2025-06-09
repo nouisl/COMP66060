232951b574b7aa323f0e6e601c0f877ff6e746af
nouisl <78652710+nouisl@users.noreply.github.com>
2025-06-09 23:57:22 +0100

    applying web3context and routing in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ProviderWrapper } from './context/Web3Context';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Web3ProviderWrapper>
+        <App />
+      </Web3ProviderWrapper>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
