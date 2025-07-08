b361ef923b8503fefe9749816fedf2ef1ed08351
nouisl <78652710+nouisl@users.noreply.github.com>
2025-07-08 13:57:09 +0100

    Pinata integration - IPFS

---
+++ frontend/src/utils/pinata.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const PINATA_API_KEY = process.env.REACT_APP_PINATA_API_KEY;
+const PINATA_SECRET_API_KEY = process.env.REACT_APP_PINATA_API_SECRET;
+
+export async function uploadFileToPinata(file) {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  const data = new FormData();
+  data.append('file', file);
+
+  const res = await axios.post(url, data, {
+    maxContentLength: 'Infinity',
+    headers: {
+      pinata_api_key: PINATA_API_KEY,
+      pinata_secret_api_key: PINATA_SECRET_API_KEY,
+    },
+  });
+
+  return res.data.IpfsHash;
+}
