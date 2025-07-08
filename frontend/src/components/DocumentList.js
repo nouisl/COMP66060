628008b7970ab408ba8664960efd2ea6153aee44
nouisl <78652710+nouisl@users.noreply.github.com>
2025-07-08 11:15:58 +0100

    page filler

---
+++ frontend/src/components/DocumentList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DocumentList() {
+  return (
+    <div>
+      <h2>Your Documents</h2>
+      <p>This is the document list/dashboard!</p>
+    </div>
+  );
+}
+
+export default DocumentList;
