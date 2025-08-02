9b434813f62d6ada1dcb55e43f7486cc82dda747
nouisl <78652710+nouisl@users.noreply.github.com>
2025-08-02 13:53:48 +0100

    added comments to private routes component

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,13 +1,17 @@
+// imports
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Web3Context } from '../context/Web3Context';
 
+// PrivateRoute component
 function PrivateRoute({ children }) {
   const { isRegistered } = useContext(Web3Context);
+  // redirect to register if user not registered
   if (!isRegistered) {
     return <Navigate to="/register" replace />;
   }
   return children;
 }
 
+// export the PrivateRoute component
 export default PrivateRoute; 
